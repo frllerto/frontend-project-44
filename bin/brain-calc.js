fafbbc343e59e27a6ee53ca069413e3929247b78
Ekaterina M <mks78spb@gmail.com>
2022-12-16 22:45:22 +0300

    modify files

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,30 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import brainCalc from '../src/games/brain-calc-0.js';
 
-console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
-console.log('What is the result of the expression?');
-let winCount = 0;
-const operators = [];
-operators.push('+', '-', '*');
-for (let i = 0; i < 3; i += 1) {
-    const randomNumber1 = Math.round(Math.random() * 100);
-    const randomNumber2 = Math.round(Math.random() * 100);
-    const operator = operators[Math.floor(Math.random() * 3)];
-    const expression = `${randomNumber1} ${operator} ${randomNumber2}`;
-    console.log(`Question: ${expression}`);
-    const answer = readlineSync.question('Your answer: ');
-    const correctAnswer = eval(expression);
-    if (Number(answer) === correctAnswer) {
-        console.log('Correct!');
-        winCount += 1;
-    } else {
-        console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
-        console.log(`Let's try again, ${name}!`);
-        break;
-    }
-}
-if (winCount === 3) {
-    console.log(`Congratulations, ${name}!`);
-}
+brainCalc();
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,29 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import brainEven from '../src/games/brain-even-0.js';
 
-console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-let winCount = 0;
-for (let i = 0; i < 3; i += 1) {
-    const randomNumber = Math.round(Math.random() * 1000);
-    const isEven = (randomNumber % 2 === 0);
-    console.log(`Question: ${randomNumber}`);
-    const answer = readlineSync.question('Your answer: ');
-    if ((answer === 'yes' && isEven === true) || (answer === 'no' && isEven === false)) {
-        console.log('Correct!');
-        winCount += 1;
-    } else {
-        if (answer === 'yes') {
-            console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'`);
-        } else if (answer === 'no') {
-            console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'`);
-        }
-        console.log(`Let's try again, ${name}!`);
-        break;
-    }
-}
-if (winCount === 3) {
-    console.log(`Congratulations, ${name}!`);
-}
+brainEven();
